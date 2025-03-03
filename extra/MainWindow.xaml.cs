5495296d7eb618ca992d68fa5c5e8a0215e55fb9
NukuHack <109027819+NukuHack@users.noreply.github.com>
2025-03-03 23:44:44 +0100

    just to have 'em all

--- extra/MainWindow.xaml.cs
+++ extra/MainWindow.xaml.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
+using System.IO;
 using System.Text;
+using System.Linq;
+using System.Drawing;
+using System.Xml.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Drawing;
-using System.IO;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Threading;
+using System.Windows.Navigation;
+using System.Windows.Media.Imaging;
 
 namespace color
 {
